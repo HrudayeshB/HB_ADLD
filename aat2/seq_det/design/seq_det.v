03ef06a76e56a3688d56098b76bbfe9340a50cfc
HrudayeshB <hrudayesh.b@gmail.com>
2025-01-04 21:08:18 +0530

    seq det fixed icarus

--- aat2/seq_det/design/seq_det.v
+++ aat2/seq_det/design/seq_det.v
@@ -4,22 +4,23 @@ parameter [1:0] A=2'b00, B=2'b01, C=2'b10, D=2'b11;
 reg [1:0] PS, NS;
 
 always @ (posedge clk, negedge rst_n) begin
-	PS <= (!rst_n) ? A : NS;
+	if (!rst_n) begin PS <= A;
+	out <= 1'b0; end
+	else PS <= NS;
 end
 
-always @ (in or PS) begin 
+always @ (*) begin 
+	NS = A;
+	out = 1'b0;
+
 	case(PS)
-
-	A: if (in) begin NS <= B; out <= 0; end else begin NS <= A; out <= 0; end// reset
-	B: if (in) begin NS <= D; out <= 0; end else begin NS <= C; out <= 0; end  // '1' detected
-	C: if (in) begin NS <= B; out <= 1; end else begin NS <= A; out <= 0; end // '10' detected
-	D: if (in) begin NS <= B; out <= 0; end else begin NS <= A; out <= 1; end  // '11' detected
-	default: begin NS <= A; out <= 0; end
-
+	A: if (in) begin NS = B; out = 1'b0; end else begin NS = A; out = 1'b0; end// reset
+	B: if (in) begin NS = D; out = 1'b0; end else begin NS = C; out = 1'b0; end  // '1' detected
+	C: if (in) begin NS = B; out = 1'b1; end else begin NS = A; out = 1'b0; end // '10' detected
+	D: if (in) begin NS = D; out = 1'b0; end else begin NS = A; out = 1'b1; end  // '11' detected
+	default: begin NS = A; out = 1'b0; end
 	endcase
 end
 
 endmodule 
-
-
 
--- aat2/seq_det/testbench/tb.v
+++ aat2/seq_det/testbench/tb.v
@@ -1,7 +1,6 @@
 module tb;
     reg clk, rst_n, in;
     wire out;
-    integer i;
 
     seq_det uut (clk, rst_n, in, out);
     
@@ -11,7 +10,8 @@ module tb;
         $dumpvars(0, tb);
         clk = 1'b0;  // Start with clock low
         rst_n = 1'b0;  // Apply reset
-        #10 rst_n = 1'b1;  // Deassert reset after 3 time units 
+        in = 1'b0;
+        #3 rst_n = 1'b1;  // Deassert reset after 3 time units 
     end
 
 
@@ -20,28 +20,27 @@ module tb;
 
     // Input stimulus
     initial begin
-        #5;
-        in = 0; #10  // Idle state
-        in = 1; #10  // Start sequence
-        in = 0; #10  // '10'
-        in = 1; #10  // '101' detected
-        in = 1; #10  // Overlapping '110' detected
-        in = 0; #10  // Resetting to '10'
-        in = 1; #10  // Another '101'
-        in = 0; #10  // Start of '101' again
-        in = 1; #10  // Overlapping '101' detected
-        in = 0; #10
+        @(posedge clk); in = 1'b0; // Idle state
+        @(posedge clk); in = 1'b1;  // Start sequence
+        @(posedge clk); in = 1'b0;  // '10'
+        @(posedge clk); in = 1'b1;  // '101' detected
+        @(posedge clk); in = 1'b1;  // Overlapping '110' detected
+        @(posedge clk); in = 1'b0;  // Resetting to '10'
+        @(posedge clk); in = 1'b1;  // Another '101'
+        @(posedge clk); in = 1'b0;  // Start of '101' again
+        @(posedge clk); in = 1'b1;  // Overlapping '101' detected
+        @(posedge clk); in = 1'b0; 
         $finish;     // End simulation
     end
 
     // for (i = 0; i < 20; i = i + 1) begin
     //     @ (posedge clk);
-    //     x = $random;    
+    //     in = $random;    
     // end
 
      // Monitor signals
     initial begin
-        $monitor("Time: %0t | clk: %b | rst: %b | x: %b | z: %b", $time, clk, rst_n, in, out);
+        $monitor("Time: %0t | clk: %b | rst: %b | in: %b | out: %b", $time, clk, rst_n, in, out);
     end
 
     // Monitor outputs every 10 time units
